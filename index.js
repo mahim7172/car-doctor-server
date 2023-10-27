b690e265003fa7986500c76443c62c05112b636a
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2023-10-28 01:11:00 +0600

    added my some changes

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
 
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
 
       const result = await serviceCollection.findOne(query, options);
@@ -49,6 +49,16 @@ async function run() {
     });
 
     // bookings
+
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingsCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const bookings = req.body;
       console.log(bookings);
